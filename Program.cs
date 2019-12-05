bba830269d040b9c54f615fd8a365c66b6616153
jlb1999 <jlbroughton88@gmail.com>
2019-12-05 14:59:49 -0500

    Initial check-in of module NumberGuesser

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+// Namespace
+namespace NumberGuesser
+{
+    // Main Class
+    class Program
+    {
+        // Entry Point Method
+        static void Main(string[] args)
+        {
+            // Set app vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Jacob Broughton";
+
+            // Change text color
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("{0}: Version {1}. Created by {2}", appName, appVersion, appAuthor);
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("What is your name?");
+
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hello, {0}. Lets play a game.", inputName);
+
+            // Init correct number
+            int correctNumber = 7;
+
+            // Init guess var
+            int guess = 0;
+
+            Console.WriteLine("Guess a number between 1 and 10");
+        
+            while (guess != correctNumber)
+            {
+                string input = Console.ReadLine();
+                bool isNumber = int.TryParse(input, out guess);
+                
+                if(isNumber)
+                {
+                    guess = Int32.Parse(input);
+
+                    if (guess != correctNumber) {
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wrong number, please try again.");
+                        Console.ResetColor();
+
+                    } else {
+
+                        Console.WriteLine("Great job, you guessed it!");
+
+                    }
+                    } else {
+
+                        Console.WriteLine("Try again bucko");
+        
+                }
+            }
+        }
+    }
+}
